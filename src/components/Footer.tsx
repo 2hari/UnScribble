0169897b2e6bdd9d2804e988b09d2c1cc633ab09
2hari <two1821125run@protonmail.com>
2023-09-17 18:41:35 +0530

    feat : upload -> cloudinary. improv ux.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,36 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="flex p-4 border-t-2 text-center justify-center items-center mt-5 pb-7 sm:px-4 px-2">
-      Powered by&nbsp;
-      <Link
-        href="https://vercel.com"
-        target="_blank"
-        className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
-      >
-        Vercel
-      </Link>
-      ,&nbsp;
-      <Link
-        href="https://upload.io"
-        target="_blank"
-        className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
-      >
-        Upload
-      </Link>
-      ,&nbsp;and&nbsp;
-      <Link
-        href="https://replicate.com"
-        target="_blank"
-        className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
-      >
-        Replicate
-      </Link>
+    <footer className="flex flex-col md:flex-row p-4 border-t-2 text-center justify-center items-center mt-5 pb-7 sm:px-4 px-2">
+      <span>Powered by&nbsp;</span>
+      <div>
+        <Link
+          href="https://vercel.com"
+          target="_blank"
+          className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
+        >
+          Vercel
+        </Link>
+        ,&nbsp;
+        <Link
+          href="https://replicate.com"
+          target="_blank"
+          className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
+        >
+          Replicate
+        </Link>
+        ,&nbsp;and&nbsp;
+        <Link
+          href="https://cloudinary.com/"
+          target="_blank"
+          className="font-bold text-slate-600 hover:text-slate-900 transition-colors"
+        >
+          Cloudinary
+        </Link>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
